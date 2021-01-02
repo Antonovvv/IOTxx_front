7e010baf86fe2fc481c57878a1388e189901ab44
纪润成 <j2196863875@jirunchengdeMacBook-Pro.local>
2021-01-02 18:04:54 +0800

    新增canvas组件及移动端轨迹绘制页面

--- src/views/Record.vue
+++ src/views/Record.vue
@@ -1,10 +1,33 @@
 <template>
-  <div></div>
+  <div class="container">
+    轨迹绘制
+    <draw-canvas
+      :width="canvasWidth" :height="canvasHeight"
+      ref="drawCanvas"
+    />
+  </div>
 </template>
 
 <script>
-export default {
+import Canvas from "../components/Canvas.vue";
 
+export default {
+  data() {
+    return {
+      canvasWidth: 300,
+      canvasHeight: 150,
+    }
+  },
+  components: {
+    'draw-canvas': Canvas,
+  },
+  mounted() {
+    this.canvasWidth = document.body.clientWidth;
+    this.canvasHeight = this.canvasWidth;
+    this.$nextTick(() => {
+      this.$refs.drawCanvas.init();
+    });
+  },
 }
 </script>
 
